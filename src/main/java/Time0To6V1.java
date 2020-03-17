94a7398e102c1d72c2fa9052f2c9f03fa76adb5b
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-03-18 02:52:31 +0300

    Lesson15

---
+++ src/main/java/Time0To6V1.java
@@ -0,0 +1,27 @@
+public class Time0To6V1 {
+    public static void time() {
+        int hours = 0;
+        O: while (hours < 6) {
+            int minute = -1;
+            M: do {
+                minute++;
+                if (hours > 1 && minute % 10 == 0) {
+                    break O;
+                }
+                int second = 0;
+                I: while (second < 60) {
+                    if (second * hours > minute) {
+                        continue M;
+                    }
+                    System.out.println(hours + ":" + minute + ":" + second);
+                    second++;
+                }
+            } while (minute < 59);
+                hours++;
+        }
+    }
+
+    public static void main(String[] args) {
+        time();
+    }
+}
